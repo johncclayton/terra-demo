065a1baad31715602cf24e2ea5d21ff8fcb38fa5
John Clayton <johnc@filewave.com>
2018-11-05 08:44:46 +0100

    clarify intent with some comments

--- intro/main.tf
+++ intro/main.tf
@@ -2,12 +2,14 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "server_port_web" {}
 
+# lets use AWS and throw something into the USA region
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
     region = "eu-central-1"
 }
 
+# allow the web port and SSH on 22
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
@@ -26,17 +28,20 @@ resource "aws_security_group" "instance" {
   }
 }
 
+# fire up an instance - in this case just one
 resource "aws_instance" "web" {
     ami = "ami-030aae8cba933aede"
     instance_type = "t2.micro"
     vpc_security_group_ids = ["${aws_security_group.instance.id}"]
     
+    # pre-existing SSH key that is named; if you have the public side you can SSH using it
     key_name = "terra demo"
 
     tags {
         Name = "tf web example"
     }
 
+    # one-off init to fire up the worlds simplest hello world example
     user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
